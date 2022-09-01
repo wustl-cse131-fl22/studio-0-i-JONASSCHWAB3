962351148dbcdc06a13188890852e6eaeb7bef1e
jonas <jonas@10.232.178.29>
2022-09-01 13:39:16 -0500

    Studio 0 done!

--- src/studio0/MilageReport.java
+++ src/studio0/MilageReport.java
@@ -6,10 +6,14 @@ public class MilageReport {
 		int car1miles = 250;
 		int car2miles = 100;
 		int car3miles = 900;
+		double debugTest = 0;
 		
 		int totalMiles = car1miles + car2miles + car3miles;
 		double milesPerGallon = 24.5;
-		double totalGallons = totalMiles * milesPerGallon;
+		//System.out.println("Step 1: total miles: " + totalMiles + "miles per gallon: " + milesPerGallon);
+		debugTest = 24.5 * 1250;
+		//System.out.println("debugTest: " + debugTest);
+		double totalGallons = totalMiles / milesPerGallon;
 		double pricePerGallon = 3.05;
 		double totalPrice = totalGallons * pricePerGallon;
 		
